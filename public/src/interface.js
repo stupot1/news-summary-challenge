ccec592d8c12f4a9fb04714a1c3d6794634735b8
Stuart <scpitc@gmail.com>
2020-07-25 13:30:33 +0100

    images displayed but no positioned

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,28 +1,59 @@
-// fetch("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html")
-//   .then(response => response.json())
-//   .then(data => console.log(data.text));
+function onPageLoad(){
 
-fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=happy")
-  .then(response => response.json())
-  .then(data => cb(data))
-
-function cb(data) {
-
-  var i;
-  var li;
-  var br
+  var i, li, li2, br, webUrl, webTitle, storyImg
   var headlinesList = document.getElementById("headlinesList");
 
-  for (i = 0; i < 10; i++) {
-    li = document.createElement("a");
-    br = document.createElement("br");
-    br2 = document.createElement("br");
-    li.setAttribute('id', i);
-    li.setAttribute('href',data.response.results[i].webUrl);
-    li.appendChild(document.createTextNode(data.response.results[i].webTitle));
-    headlinesList.appendChild(li);
-    headlinesList.appendChild(br);
-    headlinesList.appendChild(br2);
-  }
-};
+  fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=happy")
+    .then(response => response.json())
+    .then(data => headlinesCallback(data))
 
+  function headlinesCallback(inputHeadlines) {
+
+    for (i = 0; i < 2; i++) {
+      webUrl = inputHeadlines.response.results[i].webUrl
+      webTitle = inputHeadlines.response.results[i].webTitle
+      apiUrl = "http" + inputHeadlines.response.results[i].apiUrl.substr(5)
+      li = document.createElement("a");
+      
+      br = document.createElement("br");
+      br2 = document.createElement("br");
+      li.setAttribute('id', i);
+      li.setAttribute('href', webUrl);
+      li.appendChild(document.createTextNode(webTitle));
+      headlinesList.appendChild(li);
+
+      fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=" + apiUrl + "?show-fields=all")
+        .then(response => response.json())
+        .then(data => storyCallback(data))
+    
+        headlinesList.appendChild(br);
+        headlinesList.appendChild(br2);
+
+    }
+  };
+
+  function storyCallback(inputStory) {
+
+  
+    storyImg = inputStory.response.content.fields.thumbnail
+
+    console.log(storyImg)
+    img = document.createElement("IMG");
+
+    img.setAttribute('id', "img" + i);
+    img.setAttribute('class', "gu-image");
+    img.src = storyImg;
+    headlinesList.appendChild(img);
+
+
+  }
+
+}
+
+ 
+// console.log(data.response.content.fields.main)
+
+
+// fetch("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + url)
+// .then(response => response.json())
+// .then(data => console.log(data.text));
