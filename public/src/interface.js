669c0dd5a856d2709426a5c81d2a9e5f99b61b41
stupot1 <scpitc@gmail.com>
2020-07-25 14:45:19 +0100

    Image position sorted but text position not correct

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,56 +1,56 @@
-function onPageLoad(){
+function onPageLoad() {
 
-  var i, li, li2, br, webUrl, webTitle, storyImg
-  var headlinesList = document.getElementById("headlinesList");
+    var i, li, li2, br, webUrl, webTitle, storyImg
+    var headlinesList = document.getElementById("headlinesList");
+    var x = 0;
 
-  fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=happy")
-    .then(response => response.json())
-    .then(data => headlinesCallback(data))
-
-  function headlinesCallback(inputHeadlines) {
-
-    for (i = 0; i < 2; i++) {
-      webUrl = inputHeadlines.response.results[i].webUrl
-      webTitle = inputHeadlines.response.results[i].webTitle
-      apiUrl = "http" + inputHeadlines.response.results[i].apiUrl.substr(5)
-      li = document.createElement("a");
-      
-      br = document.createElement("br");
-      br2 = document.createElement("br");
-      li.setAttribute('id', i);
-      li.setAttribute('href', webUrl);
-      li.appendChild(document.createTextNode(webTitle));
-      headlinesList.appendChild(li);
-
-      fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=" + apiUrl + "?show-fields=all")
+    fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=happy")
         .then(response => response.json())
-        .then(data => storyCallback(data))
-    
-        headlinesList.appendChild(br);
-        headlinesList.appendChild(br2);
+        .then(data => headlinesCallback(data))
 
+    function headlinesCallback(inputHeadlines) {
+
+        for (i = 1; i < 3; i++) {
+            webUrl = inputHeadlines.response.results[i].webUrl
+            webTitle = inputHeadlines.response.results[i].webTitle
+            apiUrl = "http" + inputHeadlines.response.results[i].apiUrl.substr(5)
+            li = document.createElement("a");
+
+            br = document.createElement("br");
+            br2 = document.createElement("br");
+            li.setAttribute('id', i);
+            li.setAttribute('href', webUrl);
+            console.log((250 + (i * 250))) + "px";
+            li.style.top = (250 + (i * 250)) + "px";
+            console.log(li.style.top)
+            li.appendChild(document.createTextNode(webTitle));
+            headlinesList.appendChild(li);
+
+            fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=" + apiUrl + "?show-fields=all")
+                .then(response => response.json())
+                .then(data => storyCallback(data))
+
+            headlinesList.appendChild(br);
+            headlinesList.appendChild(br2);
+
+        }
+    };
+
+    function storyCallback(inputStory) {
+
+        storyImg = inputStory.response.content.fields.thumbnail
+        img = document.createElement("IMG");
+        img.setAttribute('id', "img" + x);
+        img.setAttribute('class', "gu-image");
+        img.style.top = (-x * 100);
+        img.src = storyImg;
+        headlinesList.appendChild(img);
+        x++
     }
-  };
-
-  function storyCallback(inputStory) {
-
-  
-    storyImg = inputStory.response.content.fields.thumbnail
-
-    console.log(storyImg)
-    img = document.createElement("IMG");
-
-    img.setAttribute('id', "img" + i);
-    img.setAttribute('class', "gu-image");
-    img.src = storyImg;
-    headlinesList.appendChild(img);
-
-
-  }
 
 }
 
- 
+
 // console.log(data.response.content.fields.main)
 
 
