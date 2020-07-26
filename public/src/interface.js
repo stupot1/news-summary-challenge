2a45d8b534964ecfe1d926926730b3025e5b28aa
Stuart <scpitc@gmail.com>
2020-07-26 21:55:35 +0100

    Added background

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,15 +1,15 @@
 function onPageLoad() {
 
-    var i, li, br, j
-    var webUrl = []
-    var webTitle = []
-    var storyImg = []
-    var apiUrl = []
-    var headlinesList = document.getElementById("headlinesList");
-    var x = 0
-    const numberOfStories = 10
+  var i, li, br, j
+  var webUrl = []
+  var webTitle = []
+  var storyImg = []
+  var apiUrl = []
+  var headlinesList = document.getElementById("headlinesList");
+  var x = 0
+  const numberOfStories = 10
 
-    fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=happy")
+    fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=")
         .then(response => response.json())
         .then(data => headlinesCallback(data))
 
@@ -27,8 +27,8 @@ function onPageLoad() {
     };
 
     function storyCallback(inputStory) {
+      
         storyImg[x] = inputStory.response.content.fields.thumbnail
-        console.log("x" + x)
         x++
 
         if (x==numberOfStories){
@@ -36,17 +36,19 @@ function onPageLoad() {
             for (j = 0; j < numberOfStories; j++) {
 
                 img = document.createElement("IMG");
-                img.setAttribute('id', "img" +j);
-                img.setAttribute('class', "gu-image");
-                img.src = storyImg[j];
-                headlinesList.appendChild(img);
-                
                 li = document.createElement("a");
                 br = document.createElement("br");
                 br2 = document.createElement("br");
+
+                img.setAttribute('id', "img" +j);
+                img.setAttribute('class', "gu-image");
+                img.src = storyImg[j];
+                
                 li.setAttribute('id', j);
                 li.setAttribute('href', webUrl[j]);
                 li.appendChild(document.createTextNode(webTitle[j]));
+
+                headlinesList.appendChild(img);
                 headlinesList.appendChild(li);
                 headlinesList.appendChild(br);
                 headlinesList.appendChild(br2);
@@ -54,11 +56,6 @@ function onPageLoad() {
         }
     }
 }
-
-
-
-// console.log(data.response.content.fields.main)
-
 
 // fetch("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + url)
 // .then(response => response.json())
